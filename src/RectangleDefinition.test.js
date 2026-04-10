7694c8db336c5d329bc8bc59af858d41206a723b
Philip Johnson <p.zhao.johnson@gmail.com>
2026-04-10 18:13:11 -0400

    Add RectangleDefinition class for internal use

---
+++ src/RectangleDefinition.test.js
@@ -0,0 +1,29 @@
+import { RectangleDefinition } from './RectangleDefinition';
+
+describe('`class RectangleDefinition`', () => {
+  test('`toString()`', () => {
+    var d = new RectangleDefinition();
+
+    d.centerX = 30;
+    d.centerY = 20;
+
+    d.direction = -0.5 - (Math.PI / 2);
+
+    d.width = 100;
+    d.height = 42;
+
+    d.cornerRadius = 18;
+
+    expect(d.toString()).toBe(
+      'M 48.01470566980367 -13.770851035032322'
+      + ' A 18 18 90 0 1 72.44085147870604 -6.604024615881267'
+      + ' L 75.31740471033126 -1.338529244539031'
+      + ' A 18 18 90 0 1 68.15057829118021 23.087616564363334'
+      + ' L 11.98529433019636 53.77085103503234'
+      + ' A 18 18 90 0 1 -12.440851478706008 46.604024615881286'
+      + ' L -15.317404710331227 41.338529244539046'
+      + ' A 18 18 90 0 1 -8.150578291180175 16.912383435636677'
+      + ' Z'
+    );
+  });
+});
---
+++ src/RectangleDefinition.ts
@@ -0,0 +1,88 @@
+import { Point } from '@rnacanvas/points.oopified';
+
+export class RectangleDefinition {
+  centerX = 0;
+  centerY = 0;
+
+  /**
+   * Have rectangles be upright by default.
+   */
+  direction = -Math.PI / 2;
+
+  width = 0;
+  height = 0;
+
+  cornerRadius = 0;
+
+  /**
+   * Returns an SVG path definition.
+   */
+  toString(): string {
+    let p = new Point(this.centerX, this.centerY);
+
+    p.displace({
+      magnitude: this.height / 2,
+      direction: this.direction,
+    });
+
+    p.displace({
+      magnitude: (this.width / 2) - this.cornerRadius,
+      direction: this.direction + (Math.PI / 2),
+    });
+
+    let d = `M ${p.x} ${p.y}`;
+
+    p.displace({
+      magnitude: Math.SQRT2 * this.cornerRadius,
+      direction: this.direction + (3 * Math.PI / 4),
+    });
+
+    d += ` A ${this.cornerRadius} ${this.cornerRadius} 90 0 1 ${p.x} ${p.y}`;
+
+    p.displace({
+      magnitude: this.height - (2 * this.cornerRadius),
+      direction: this.direction + Math.PI,
+    });
+
+    d += ` L ${p.x} ${p.y}`;
+
+    p.displace({
+      magnitude: Math.SQRT2 * this.cornerRadius,
+      direction: this.direction + (5 * Math.PI / 4),
+    });
+
+    d += ` A ${this.cornerRadius} ${this.cornerRadius} 90 0 1 ${p.x} ${p.y}`;
+
+    p.displace({
+      magnitude: this.width - (2 * this.cornerRadius),
+      direction: this.direction + (3 * Math.PI / 2),
+    });
+
+    d += ` L ${p.x} ${p.y}`;
+
+    p.displace({
+      magnitude: Math.SQRT2 * this.cornerRadius,
+      direction: this.direction + (7 * Math.PI / 4),
+    });
+
+    d += ` A ${this.cornerRadius} ${this.cornerRadius} 90 0 1 ${p.x} ${p.y}`;
+
+    p.displace({
+      magnitude: this.height - (2 * this.cornerRadius),
+      direction: this.direction,
+    });
+
+    d += ` L ${p.x} ${p.y}`;
+
+    p.displace({
+      magnitude: Math.SQRT2 * this.cornerRadius,
+      direction: this.direction + (9 * Math.PI / 4),
+    });
+
+    d += ` A ${this.cornerRadius} ${this.cornerRadius} 90 0 1 ${p.x} ${p.y}`;
+
+    d += ' Z';
+
+    return d;
+  }
+}
