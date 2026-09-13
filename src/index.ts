7d76f2111b89386b9559b784879beda65618dd23
Philip Johnson <p.zhao.johnson@gmail.com>
2026-09-13 14:02:53 -0400

    Add missing exports for strung element classes

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,15 @@ export { Rectangle };
 
 import { Triangle } from './Triangle';
 export { Triangle };
+
+import { StrungText } from './StrungText';
+export { StrungText };
+
+import { StrungCircle } from './StrungCircle';
+export { StrungCircle };
+
+import { StrungRectangle } from './StrungRectangle';
+export { StrungRectangle };
+
+import { StrungTriangle } from './StrungTriangle';
+export { StrungTriangle };
